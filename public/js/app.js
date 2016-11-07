bb94765383591d95650ad08204554fbb5037cafa
Che Mok <chemok78@gmail.com>
2016-11-07 09:58:42 +0000

    created update user function an view

--- public/js/app.js
+++ public/js/app.js
@@ -11,9 +11,21 @@ angular.module("booksApp", ['ngRoute'])
     .when("/mybooks", {
         
        templateUrl: "mybooks.html",
-       controller: "MybooksController"
+       controller: "MyBooksController"
         
-    });
+    })
+    .when("/allbooks", {
+      
+      templateUrl: "allbooks.html",
+      controller: "AllBooksController"
+        
+    })
+    .when("/profile", {
+        
+      templateUrl: "profile.html",
+      controller: "ProfileController"
+        
+    })
     
 })
 .service("Books", function($http){
@@ -28,36 +40,176 @@ angular.module("booksApp", ['ngRoute'])
     
     
 })
+.service("Login", function($http){
+    
+    this.isLoggedIn = function(){
+      
+      return $http.get("/loggedin");  
+        
+        
+    };
+    
+    
+})
+.service("Users", function($http){
+    
+    this.editUser = function(userData){
+        
+        return $http.post("/edituser", userData)
+        //send a http post request to /edituser route Node JS server
+            .then(function(response){
+                
+                return response;
+                
+            }, function(response){
+                
+                console.log("Error editing user");          
+                
+            });
+        
+    };//this.editUser
+    
+})
+.controller("mainController", function($scope, $rootScope, Login){
+//Main controller is loaded on page load or after Passport login    
+    
+    Login.isLoggedIn()
+    //call service that checks on server side if a user is logged in, when controller is loaded
+        .then(function(response){
+        //server returns '0' when there is no user authenticated 
+        //server returns 'req.user' when there is a user authenticated. We use req.user to attach user properties to scope
+            
+            if(response.data == '0'){
+            //no user is logged in    
+            
+                console.log("no user is logged in");
+                
+                $scope.loggedIn = false;
+                
+            } else {
+                
+                console.log("we found a user");
+                console.log(response.data);
+                
+                
+                $scope.userObject = response.data;
+                delete $scope.userObject._id;
+                //delete ._id created by MongoDB from userObject
+                $scope.loggedIn = true;
+                //$scope.userID = response.data.id;
+                //$scope.displayName = response.data.name;
+                //$scope.city = response.data.city;
+                //$scope.state = response.data.state;
+                //$scope.books = response.data.books;
+                
+            }
+            
+            
+        }, function(response){
+            
+            console.log("error checking if a user is logged in");
+            
+            return response;
+            
+        });
+    
+    
+    
+})
 .controller('HomeController', function($scope){
 //controller for the homepage
     
     console.log("home controller!");
     
 })
-.controller('MybooksController', function($scope, Books){
+.controller('MyBooksController', function($scope, Books){
 //controller for the Mybooks profile page
-
-        console.log("Mybooks Controller!");
 
         $scope.addBook = function(query){
             
             Books.retrieveBook(query)
             
                 .then(function(response){
-                    
-                    console.log(response);
-                    
-                    $scope.book = response;
+                
+                    var bookObject = {
+                        
+                        "title": response.data[0].title,
+                        "subtitle": response.data[0].subtitle,
+                        "authors": response.data[0].authors,
+                        "description": response.data[0].description,
+                        "link": response.data[0].link,
+                        "thumbnail": response.data[0].thumbnail
+                    };
+
+                    $scope.book = bookObject;
+                    //array of objects, every book is an object
                     
                 }, function(response){
                     
-                    
                     console.log("failed to retrieve books");
                     
-                })
+                });
             
             
         };
 
     
-});
+})
+.controller('AllBooksController', function($scope){
+    
+    
+    
+    
+    
+})
+.controller('ProfileController', function($scope, Users){
+    
+    $scope.updateUser = function(userData){
+    //get the new city and state from front-end form as useData parameter
+    //{{"city": "string", "state": "string"}}
+        
+        $scope.userObject.city = userData.city;
+        $scope.userObject.state = userData.state;
+        //update $scope properties of userObject to userData
+        
+        Users.editUser($scope.userObject).then(function(response){
+        //call editUser service with edited userObject from scope
+        
+            console.log("user updated");
+            
+        }, function(response){
+            
+            console.log(response);
+            
+        });
+        
+    };//$scope.updateUser
+    
+})
+.service('authInterceptor', function($q){
+ //service to intercept a 401 response from Express REST API if user is not authenticated for a protected endPoint 
+ 
+    var service = this;
+    
+    service.responseError = function(response){
+    //make a authIntercepter.responseError() method that takes a server response 
+    
+        if(response.status == 401){
+        //if response error status is 401 redirect to login URL 
+        
+            window.location = "/auth/facebook";
+            
+        }
+        
+        return $q.reject(response);
+        //if the response error status is something other than 401 reject the promise with the response
+        
+    };
+
+    
+})
+ .config(['$httpProvider', function($httpProvider) {
+    //add authInterceptor service to httpProvider so its used in    
+        
+        $httpProvider.interceptors.push('authInterceptor');
+    }]);
--- server.js
+++ server.js
@@ -15,7 +15,24 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 //use native mongoDB drive, change to Mongoose later
 
+var ObjectID = mongodb.ObjectID;
+//load ObjectID method so we can generate new objectID, using objectId = new ObjectID
+//ObjectId is a 12-byte BSON type
+//MongoDB uses ObjectIds as the default value of _id field of each document which is generated while creation of any document
+
+var passport = require("passport");
+//load passport.js
+
+var FacebookStrategy = require('passport-facebook').Strategy;
+//load passport.js Facebook strategy
+
+var session = require("express-session");
+//load express session
+
 var BOOKS_COLLECTION = "books";
+
+var books = require('google-books-search');
+//load Google Books API wrapper for Node JS
 
 var app = express();
 
@@ -25,10 +42,6 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 //parse all requests as JSON in the app instance
 
-var books = require('google-books-search');
-//load Google Books API wrapper for Node JS
-
-var db;
 
 mongodb.MongoClient.connect(process.env.DB_URL, function(err, database) {
 
@@ -68,38 +81,289 @@ mongodb.MongoClient.connect(process.env.DB_URL, function(err, database) {
 
   } //function handleError
 
+  app.use(session({
+    secret: 'keyboard cat'
+  }));
+  //use sessions in Express app instance
 
-  app.get("/getbook/:query", function(req,res){
-  //retrieve data from Google Books API using the query URL parameter  
-  
-    var options = {
+  app.use(passport.initialize());
+  //initialize passport in Express app instance
+
+  app.use(passport.session());
+  //initialize passport sessions and use it in Express app instance
+
+  var db;
+
+  passport.use(new FacebookStrategy({
+    //use Facebook strategy with Passport instance
+
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK_DEV
+
+  }, function(accessToken, refreshToken, user, done) {
+    //callback function after successful login
+
+    process.nextTick(function() {
       
-      limit: 1
-      
-    };
-      
-     books.search(req.params.query, options, function(error,results){
-         
-         if(error){
-             
-             console.log(error);
-             
-         } else {
-             
-             console.log(results);
-             
-             res.status(200).json(results);
-             
-             
-         }
-         
-         
-     });
+      db.collection(BOOKS_COLLECTION).findOne({id: user.id}, function(err,doc){
+      //check if user exists in database, find with user.id  
         
+        if(err){
+        //error handling finding user in database
+        
+          console.log("error finding user in database");
+          
+        } else {
+          
+          if(doc == null){
+          //if doc from reading database is null, user does not exist yet in DB
+          
+           var userObject = user._json;
+           
+           userObject.city = "";
+           userObject.state = "";
+           userObject.books = [];  
+            
+            db.collection(BOOKS_COLLECTION).insertOne(userObject, function(err,doc){
+            //insert user in database
+            
+              if(err){
+                
+                console.log("error inserting user in database");
+                
+              } else{
+                
+                console.log("user inserted in database");
+                
+              }
+              
+              
+            }); // db.collection(BOOKS_COLLECTION).insertOne
+            
+          } //if (doc == null)
+          
+        } //if, else
+        
+        
+      }); // db.collection(BOOKS_COLLECTION).findOne
+
+      done(null, user);
+      //return user profile after successfull login
+      //if the credentials are valid, the verify callback invokes done to supply Passport with the user that authenticated.
+
+      /*
+      {
+        _id: '',
+        id: '',
+        displayName: 'Che Mok',
+        state: '',
+        city: '',
+        books: [
+          
+          {title: 'javascript', author: 'John Doe', thumbnail: ''},
+          
+          {title: 'javascript', author: 'John Doe', thumbnail: ''}
+          
+        
+        ]
+        
+      }
+     
+      */
+
+    }); //process.nextTick
+
+
+  })); //passport.use
+
+  passport.serializeUser(function(user, done) {
+    //save user object in session
+    //result of serializeUser is attached to the session as req.session.passport.user = {};   
+    //http://stackoverflow.com/questions/27637609/understanding-passport-serialize-deserialize
+
+    console.log("user serialized!");
+
+    console.log(user);
+
+    done(null, user);
+    //can also be done(null,user.id) if you want to save only the id
+
+
+  });
+
+
+  passport.deserializeUser(function(id, done) {
+    //retrieve with the key given as obj parameter
+    //the fetched object will be attached to req.user
+
+    console.log("user deserialized");
+
+    console.log(id);
+
+    done(null, id);
+
+  });
+
+  /*Facebook Routes*/
+
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+  //authenticate with facebook when hitting this route
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    //Handle callback after successfully authenticated with Facebook  
+
+    successRedirect: process.env.FACEBOOK_SUCCESS_REDIRECT_DEVELOPMENT,
+
+    failureRedirect: '/error'
+
+  }));
+
+
+
+  app.get('/test', function(req, res, next) {
+
+    //route for testing authentication  
+
+    console.log("check error");
+
+    console.log(req.user);
+
+    console.log(req.isAuthenticated());
+
+    res.send("Error logging in");
+
+
+  });
+
+
+  var auth = function(req, res, next) {
+    //middleware function to check if user is logged in for every Express request
+    //used in Express routes
+
+    if (!req.isAuthenticated()) {
+
+      console.log("You need to login!");
+
+      res.sendStatus(401);
+      //send a 401, that will be handled by authInterceptor in Angular JS
+
+
+    } else {
+
+
+      console.log("You are logged in");
+
+      next();
+
+    }
+
+
+
+  };
+
+
+  app.get('/loggedin', function(req, res) {
+    //route to test if a user is authenticated  
+    //is always called when mainController is loaded, so also after Passport login
+
+    if (req.isAuthenticated()) {
+      //a user is autenticated  
+      //retrieve user object from database
       
-      
+      db.collection(BOOKS_COLLECTION).findOne({id: req.user.id}, function(err,doc){
+        
+        if(err){
+          
+          handleError(res,err.message, "Failed to retrieve user from database");
+          
+        } else {
+          
+
+          res.status(200).json(doc);
+          
+          
+        }
+        
+        
+      }); //db.collection(BOOKS_COLLECTION)
+
+
+    } else {
+      //no user is authenticated and send a '0' response to front-end  
+
+      res.send('0');
+
+    }
+
+
+  });
+
+  app.get('/logout', function(req, res) {
+    //route to log out user from Facebook login
+
+    req.logout();
+    //use Passport JS build in method to log user out and deserialize user
+
+    res.redirect('/');
+    //redirect to homepage after log out
+
   });
   
+  app.post('/edituser', function(req,res){
+    
+    console.log(req.body);
+    
+    db.collection(BOOKS_COLLECTION).updateOne({id: req.user.id}, req.body, function(err,doc){
+      
+      if(err){
+        
+        handleError(res,err.message, "Failed to update user");
+        
+      } else{
+      
+        
+        res.status(200).end();
+        
+      }
+      
+      
+    });//db.collection
+    
+  });
+
+
+  app.get("/getbook/:query", function(req, res) {
+    //retrieve data from Google Books API using the query URL parameter  
+
+    var options = {
+
+      limit: 1
+
+    };
+
+    books.search(req.params.query, options, function(error, results) {
+
+      if (error) {
+
+        console.log(error);
+
+      } else {
+
+        console.log(results);
+
+        res.status(200).json(results);
+
+
+      }
+
+
+    });
+
+
+
+  });
+
 
 }); //mongodb.MongoCLient.connect
 
